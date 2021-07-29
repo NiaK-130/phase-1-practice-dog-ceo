e1067df3e492156be220065569db94de00583944
Nia K <nsjkappen@gmail.com>
2021-07-28 19:57:11 -0700

    commit tree a29e619ddca03dfcbe1dc6cdeb577d118f549506

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl).then(resp => resp.json()).then(dogceo => {
+    dogceo.forEach(imgUrl)
+})
